026b9d44f5d4587efc8ea6e961876f1d3fedd726
tearcode89 <sjun8932.3@gmail.com>
2023-08-04 20:48:51 +0900

    15. 스크롤 애니메이션 구현 4

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
     let yOffset = 0; // window.pageYOffset 대신 쓸 변수
     let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0; // 현재 활성화된 (눈 앞에 보고있는) scene(scroll-section)
+    let enterNewScene = false; // 새로운 scene이 시작된 순간 true 
 
     const sceneInfo = [
         {
@@ -72,8 +73,8 @@
 
     function calcValues (values, currentYOffset) {
         let rv;
-        // 현재 scene(스크롤 섹션)에서 스크롤된 범위를 비율로 구하
-        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight
+        // 현재 scene(스크롤 섹션)에서 스크롤된 범위를 비율로 구하기
+        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
 
         rv = scrollRatio * (values[1] - values[0]) + values[0]; 
 
@@ -85,11 +86,14 @@
         const values = sceneInfo[currentScene].values
         const currentYOffset = yOffset - prevScrollHeight
 
+        console.log(currentScene);
+
         switch (currentScene) {
             case 0:
                 // console.log('0 play')
                 let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset)
                 objs.messageA.style.opacity = messageA_opacity_in;
+                console.log(messageA_opacity_in)
                 break;
 
             case 1:
@@ -107,22 +111,27 @@
     }
 
     function scrollLoop() {
+        enterNewScene = false;
         prevScrollHeight = 0;
         for (let i = 0; i < currentScene; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
         if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene ++;
             document.body.setAttribute('id', `show-scene-${currentScene}`)
         }
 
         if(yOffset < prevScrollHeight) {
+            enterNewScene = true;
             if (currentScene === 0) return;
             currentScene --;
             document.body.setAttribute('id', `show-scene-${currentScene}`)
         }
 
+        if (enterNewScene === true) return;
+        
         playAnimation();
     }
 
