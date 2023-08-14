906ad55a38744abaaf8982c5245e7374143110c2
tearcode89 <sjun8932.3@gmail.com>
2023-08-14 09:30:15 +0900

    35. 이미지 블렌딩 1

--- js/main.js
+++ js/main.js
@@ -112,6 +112,7 @@
             values: {
                 rect1X: [ 0, 0, { start: 0, end: 0} ],
                 rect2X: [ 0, 0, { start: 0, end: 0} ],
+                imageBlendY: [ 0, 0, { start: 0, end: 0}],
                 rectStartY: 0
             }
         },
@@ -408,6 +409,9 @@
                     step = 2;
                     // console.log('캔버스 닿은 후')
                     // 이미지 블렌드
+                    // imageBlendY: [0, 0, {start: 0,end: 0}]
+                    objs.context.drawImage(objs.images[1], 0, 200);
+
                     objs.canvas.classList.add('sticky')
                     objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`
                 }
